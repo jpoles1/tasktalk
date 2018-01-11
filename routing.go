f395f61048de8d743b505aa85698e63da288e499
Jordan Poles <jpoles1@gmail.com>
2018-01-10 19:11:43 -0500

    Debug router.

--- routing.go
+++ routing.go
@@ -1,7 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func hello(w http.ResponseWriter, r *http.Request) {
+	urlparams := mux.Vars(r)
+	log.Println(r, urlparams)
 	w.Write([]byte("Hello World!"))
 }
